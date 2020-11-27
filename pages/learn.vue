c0e23afe94e6ba0a497c174be158564745b3a755
João Daniel <jotaf.daniel@gmail.com>
2020-11-27 14:15:35 -0300

    add placeholder learn page
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ pages/learn.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container mx-auto">
+    <main class="h-screen w-full px-4">
+      <h1 class="text-xl lg:text-4xl font-bold py-4">The Microservices Info</h1>
+      <h2 class="text-lg lg:text-2xl py-2">Learn more - under creation</h2>
+    </main>
+  </div>
+</template>
