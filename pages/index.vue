4f9fa7a87b3a5a8e72d4199069acff6536466d04
João Daniel <jotaf.daniel@gmail.com>
2020-11-28 20:14:40 -0300

    add logo and favicon
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto h-screen flex flex-col justify-center">
-    <h1
-      class="mb-10 lg:mb-20 font-serif lg:w-1/3 mx-auto text-center font-bold text-5xl lg:text-6xl"
-    >
-      The Microservices Info
-    </h1>
+    <img
+      src="~/assets/logo.png"
+      alt="The Microservices Info"
+      class="mb-10 w-2/3 lg:mb-20 font-serif lg:w-1/3 mx-auto text-center font-bold text-5xl lg:text-6xl"
+    />
 
     <div class="mt-10 flex flex-col justify-center items-center">
       <nuxt-link to="/survey" :class="takeSurvey">Take the Survey</nuxt-link>
