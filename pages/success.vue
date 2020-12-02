899e8fd75a3eaf6d2d40779bdb97a9d43455b9ee
João Daniel <jotaf.daniel@gmail.com>
2020-12-02 18:01:53 -0300

    extract success view to a proper page
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ pages/success.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container mx-auto px-4 lg:px-40 mb-20 lg:mb-40">
+    <h1 id="top" class="text-xl lg:text-2xl font-bold py-10 underline">
+      Microservices Patterns Survey
+    </h1>
+
+    <SectionCard title="Success!">
+      <div class="h-56 flex flex-col justify-center items-center">
+        <p class="lg:font-semibold">Thanks for your participation!</p>
+      </div>
+    </SectionCard>
+
+    <div class="mt-10 flex justify-center items-center">
+      <NuxtLink to="/" class="btn btn-primary">Learn more!</NuxtLink>
+    </div>
+  </main>
+</template>
--- pages/survey.vue
+++ pages/survey.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <main
-      v-if="displaySurvey"
-      class="container mx-auto px-4 lg:px-40 mb-20 lg:mb-40"
-    >
+    <main class="container mx-auto px-4 lg:px-40 mb-20 lg:mb-40">
       <h1 id="top" class="text-xl lg:text-2xl font-bold py-10 underline">
         Microservices Patterns Survey
       </h1>
@@ -29,10 +26,6 @@
 
         <a href="#top" class="btn btn-secondary">back to the top</a>
       </div>
-    </main>
-    <main v-else-if="submitSuccess">Thanks for your collaboration!</main>
-    <main v-else>
-      Something went wrong on our side, sorry for the inconvinience!
     </main>
   </div>
 </template>
@@ -67,7 +60,6 @@ export default {
   },
   data: () => ({
     submitting: false,
-    success: undefined,
 
     patterns: [
       {
@@ -141,23 +133,16 @@ export default {
       const base = ['btn', 'btn-primary']
       return this.btnDisabled ? base.concat('btn-disabled') : base
     },
-    displaySurvey() {
-      return this.success === undefined
-    },
-    submitSuccess() {
-      return this.success
-    },
   },
   methods: {
     async submit() {
       try {
         this.submitting = true
         await this.$axios.$post('/answers', this.answers)
-        this.success = true
       } catch (e) {
-        this.success = false
       } finally {
         this.submitting = false
+        this.$router.replace('/success')
       }
     },
   },
