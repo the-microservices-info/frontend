68157fa4c70b688bd73eb4c44f9c44f3fc2b0a34
João Daniel <jotaf.daniel@gmail.com>
2020-12-02 09:55:37 -0300

    add messages for submition success and failure
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- pages/survey.vue
+++ pages/survey.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="container mx-auto px-4 lg:px-40 mb-20 lg:mb-40">
-    <h1 class="text-xl lg:text-2xl font-bold py-10 underline">
-      Microservices Patterns Survey
-    </h1>
+  <div>
+    <main
+      v-if="displaySurvey"
+      class="container mx-auto px-4 lg:px-40 mb-20 lg:mb-40"
+    >
+      <h1 class="text-xl lg:text-2xl font-bold py-10 underline">
+        Microservices Patterns Survey
+      </h1>
 
-    <Introduction />
-    <BackgroundExperience />
-    <DatabasePerService />
+      <Introduction />
+      <BackgroundExperience />
+      <DatabasePerService />
 
-    <div class="mt-4 flex justify-center items-center">
-      <button :class="btnClasses" :disabled="btnDisabled" @click="submit()">
-        Submit
-      </button>
-    </div>
+      <div class="mt-4 flex justify-center items-center">
+        <button :class="btnClasses" :disabled="btnDisabled" @click="submit()">
+          Submit
+        </button>
+      </div>
+    </main>
+    <main v-else-if="submitSuccess">Thanks for your collaboration!</main>
+    <main v-else>
+      Something went wrong on our side, sorry for the inconvinience!
+    </main>
   </div>
 </template>
 
@@ -31,6 +40,7 @@ export default {
   },
   data: () => ({
     submitting: false,
+    success: undefined,
   }),
   computed: {
     ...mapGetters(['formValid', 'answers']),
@@ -41,13 +51,21 @@ export default {
       const base = ['btn', 'btn-primary']
       return this.btnDisabled ? base.concat('btn-disabled') : base
     },
+    displaySurvey() {
+      return this.success === undefined
+    },
+    submitSuccess() {
+      return this.success
+    },
   },
   methods: {
     async submit() {
       try {
         this.submitting = true
         await this.$axios.$post('/answers', this.answers)
+        this.success = true
       } catch (e) {
+        this.success = false
       } finally {
         this.submitting = false
       }
