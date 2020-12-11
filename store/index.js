d8f8b5811e59e7e40766a8f95e3ea91d9815f26d
João Daniel <jotaf.daniel@gmail.com>
2020-12-11 09:44:32 -0300

    centralize pattern data into store
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- store/index.js
+++ store/index.js
@@ -1,3 +1,102 @@
+const patterns = [
+  {
+    name: 'Database per Service',
+    paragraphs: [
+      `Each service has exclusive access to a database, therefore reducing shared data among the system. A private database stores data pertinent to the domain of its owner service.`,
+      `This pattern improves autonomy and reduces coupling between microservices, as they don’t see each other’s databases. Microservices can still share the same physical database as long as there’s a logical separation (e.g. separate schemas or tablespaces). It’s also possible that different microservices use databases of different types.`,
+    ],
+    imageSrc: require('@/assets/images/patterns/DatabasePerService.png'),
+  },
+  {
+    name: 'Saga',
+    paragraphs: [
+      `Handle transactions that span multiple services as a sequence of local transactions that, in the end, emit messages or events that trigger a transaction in another service. Also support undo operations (a.k.a. compensation) in your microservices.`,
+      `It provides a way to deal with atomicity between microservices, addressing the transaction problem, while increasing the complexity of communication`,
+    ],
+    imageSrc: require('@/assets/images/patterns/Saga.png'),
+  },
+  {
+    name: 'Event Sourcing',
+    paragraphs: [
+      `Handle the state of the system as streams of events that mark every change over the time, making possible to query events and access historical data to recover from failures.`,
+      `Instead of storing the current state of an object in the database, we store the sequence of append-only, immutable events that affected that object. Current state is obtained by replaying events, and we do so to provide a “query view” of the data.`,
+    ],
+    imageSrc: require('@/assets/images/patterns/EventSourcing.png'),
+  },
+  {
+    name: 'Asynchronous Messaging',
+    paragraphs: [
+      `Determine that communication model between internal parts of the system is based on asynchronous message-passing.`,
+      `By storing messages exchanged in a buffer, there is less need to share databases. Furthermore, it reduces the coupling between microservices, since it decouples the message senders from the recipients. Despite these advantages, it depends a lot on a message broker (which must be very resilient)`,
+    ],
+    imageSrc: require('@/assets/images/patterns/AsynchronousMessaging.png'),
+  },
+  {
+    name: 'Domain Event',
+    paragraphs: [
+      `Model business capabilities of services as DDD aggregates that emits events on every change in the state so that it can be consumed by other services`,
+      `The use of domain events and DDD terminology allows decoupling the connection between microservices based on the domain. Furthermore, when creating a collection of aggregated data, the tendency is to decrease the need to share databases between different microservices`,
+    ],
+    imageSrc: require('@/assets/images/patterns/DomainEvent.png'),
+  },
+  {
+    name: 'Transactional Outbox',
+    paragraphs: [
+      `Write events to an outbox within a local transaction when changing the state. Then another process can read the outbox and emit events to the system. This strategy overcomes when the message broker lacks support for message storage-and-forward.`,
+      `A message outbox is injected into the persistence command, based on the atomicity of the database command. The outbox is then read through a separate process to publish to a broker. `,
+    ],
+    imageSrc: require('@/assets/images/patterns/TransactionalOutbox.png'),
+  },
+  {
+    name: 'API Composition',
+    paragraphs: [
+      `Create a service dedicated to perform queries that span multiple services, in a join-like way.`,
+      `When the data is separated into different microservices, it provides a way of unified consultation. On the other hand, it creates a service that is tightly coupled with others`,
+    ],
+    imageSrc: require('@/assets/images/patterns/APIComposition.png'),
+  },
+  {
+    name: 'Service Registry',
+    paragraphs: [
+      `Create a service to keep track of addresses and copies of the rest of the services, providing an easy way to handle scalability.`,
+      `The creation of a database that stores the microservices locations instances facilitates the discovery of such instances. On the other hand, a service (Registry) dependency is created, increasing the coupling. Furthermore, in the case of Registry failure, the problem of (eventual) data consistency arises`,
+    ],
+    imageSrc: require('@/assets/images/patterns/ServiceRegistry.png'),
+  },
+  {
+    name: 'Adapter Microservice',
+    paragraphs: [
+      `Place a new microservice in between two others to adapt the provided API into the expected API.`,
+      `A new microservice -- highly -- coupled with a legacy service is added. On the other hand, a new communication interface with the rest of the system is gained, which is more flexible and consistent with the microservices architectural style`,
+    ],
+    imageSrc: require('@/assets/images/patterns/AdapterMicroservice.png'),
+  },
+  {
+    name: 'Ambassador',
+    paragraphs: [
+      `Create a new microservice that acts as a proxy dedicated to monitoring, logging, and other cross-cutting concerns and peripheral functionalities.`,
+      `Peripheral functionalities are removed from the business logic process, which increases the dependency between microservices. On the other hand, the development process is favored since a microservice is created to act as a Proxy. This pattern can be considered to be a superset of the sidecar, the difference of which is that it does not necessarily follow the life cycle of the main application`,
+    ],
+    imageSrc: require('@/assets/images/patterns/Ambassador.png'),
+  },
+  {
+    name: 'CQRS',
+    paragraphs: [
+      `Split reads and writes into separated services to possiblitate a better representation model for each operation. Each service can also scale independently from the other.`,
+      `Increases scalability and decreases the coupling between microservices. However, it creates a new service responsible for the management of the historical database, which is shared with all microservices with "interest" in the domain, and which offers eventual consistency (since the Event Sourcing pattern is generally adopted in its implementation)`,
+    ],
+    imageSrc: require('@/assets/images/patterns/CQRS.png'),
+  },
+  {
+    name: 'Self-Contained Service',
+    paragraphs: [
+      `Put the service to listen to and store events related to all data it depends, so that when requested it can simply reply without any request to other microservices.`,
+      `By definition, it is a service that depends little on others, which reduces coupling but increases the microservices internal complexity`,
+    ],
+    imageSrc: require('@/assets/images/patterns/SelfContainedService.png'),
+  },
+]
+
 const initialState = {
   introduction: {
     questions: {
@@ -25,55 +124,56 @@ const initialState = {
   },
 }
 
-const patterns = [
-  'Database per Service',
-  'Saga',
-  'Event Sourcing',
-  'Asynchronous Messaging',
-  'Domain Event',
-  'Transactional Outbox',
-  'API Composition',
-  'Service Registry',
-  'Adapter Microservice',
-  'Ambassador',
-  'CQRS',
-  'Self-Contained Service',
-]
+patterns
+  .map(({ name }) => name)
+  .forEach((pattern) => {
+    initialState[pattern] = {
+      isValid: false,
+      questions: {
+        knowledgeType: undefined,
+        isUsed: undefined,
+        statements: undefined,
+        comments: '',
+      },
+    }
+  })
 
-patterns.forEach((pattern) => {
-  initialState[pattern] = {
-    isValid: false,
-    questions: {
-      knowledgeType: undefined,
-      isUsed: undefined,
-      statements: undefined,
-      comments: '',
-    },
-  }
-})
+initialState.meta = { patterns }
 
 export const state = () => ({ ...initialState })
 
 export const getters = {
+  metaPatterns(s) {
+    return s.meta.patterns
+  },
   formValid(s) {
-    return Object.keys(s).every((section) => s[section].isValid)
+    return Object.keys(s)
+      .filter((sec) => sec !== 'meta')
+      .every((section) => s[section].isValid)
   },
   answers(s) {
-    return Object.keys(s).reduce((acc, section) => {
-      acc[section] = { ...s[section].questions }
+    return Object.keys(s)
+      .filter((sec) => sec !== 'meta')
+      .reduce((acc, section) => {
+        acc[section] = { ...s[section].questions }
 
-      if (!acc[section].isUsed) {
-        acc[section].comments = ''
-        acc[section].statements = undefined
-      }
+        const hasFalsyIsUsed =
+          Object.keys(acc[section]).includes('isUsed') && !acc[section].isUsed
 
-      return acc
-    }, {})
+        if (hasFalsyIsUsed) {
+          acc[section].comments = ''
+          acc[section].statements = undefined
+        }
+
+        return acc
+      }, {})
   },
   progress(s) {
-    const n = Object.keys(s).reduce((acc, section) => {
-      return s[section].isValid ? acc + 1 : acc
-    }, 0)
+    const n = Object.keys(s)
+      .filter((sec) => sec !== 'meta')
+      .reduce((acc, section) => {
+        return s[section].isValid ? acc + 1 : acc
+      }, 0)
 
     return n / 15
   },
