83b3072ae81da53ab16601e1d5557c838a2dc2bc
João Daniel <jotaf.daniel@gmail.com>
2020-12-11 15:44:10 -0300

    add learn scaffold for mobile
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/learn_scaffold.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <aside :class="sideMenuClasses">
+      <button class="btn btn-secondary self-end" @click="menuHidden = true">
+        close
+      </button>
+    </aside>
+    <main>
+      <button class="btn btn-secondary" @click="menuHidden = false">
+        menu
+      </button>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    menuHidden: false,
+  }),
+
+  computed: {
+    sideMenuClasses() {
+      const hiddenClasses = ['w-0', 'overflow-hidden']
+      const displayClasses = ['w-56']
+      const base = [
+        'bg-white',
+        'absolute',
+        'h-screen',
+        'shadow-lg',
+        'flex',
+        'flex-col',
+        'items-start',
+      ]
+
+      return this.menuHidden
+        ? base.concat(hiddenClasses)
+        : base.concat(displayClasses)
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+aside {
+  transition: all 0.75s ease;
+}
+</style>
