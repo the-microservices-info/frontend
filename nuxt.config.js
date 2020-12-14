1423bef5db689deb51facf36d05f73f818b7ec10
João Daniel <jotaf.daniel@gmail.com>
2020-12-14 09:57:59 -0300

    change page title
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'the-microservices-info_frontend',
+    title: 'The Microservices Info',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
