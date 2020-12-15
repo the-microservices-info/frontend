2ee7017ae5b7bd7c4f20a40753dfde2ef9dda34f
João Daniel <jotaf.daniel@gmail.com>
2020-12-15 02:52:05 -0300

    Refactor by extracting a component
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- pages/learn/Event Sourcing.vue
+++ pages/learn/Event Sourcing.vue
@@ -1,77 +1,50 @@
 <template>
-  <LearnScaffold>
-    <h1 class="text-4xl px-16 text-center font-bold">Event Sourcing</h1>
+  <PatternPage
+    name="Event Sourcing"
+    :src="require('@/assets/images/patterns/EventSourcing.png')"
+  >
+    <template #problem>
+      You are developing a system where services need to be able to access not
+      only the current state, but also need to access historical states to, say,
+      recover from a failure.
+    </template>
 
-    <div class="content">
-      <section>
-        <h2>Problem</h2>
-        <p>
-          You are developing a system where services need to be able to access
-          not only the current state, but also need to access historical states
-          to, say, recover from a failure.
-        </p>
-      </section>
+    <template #solution>
+      <strong>Store a historical sequence of events</strong> that represents
+      changes to the state, with an API that allows fetching data from any
+      moment in time.
+    </template>
 
-      <section>
-        <h2>Solution</h2>
-        <p>
-          <strong>Store a historical sequence of events</strong> that represents
-          changes to the state, with an API that allows fetching data from any
-          moment in time.
-        </p>
-
-        <img
-          src="@/assets/images/patterns/EventSourcing.png"
-          alt="Event Sourcing"
-          class="mx-auto"
-        />
-      </section>
-
-      <section>
-        <h2>Consequences</h2>
-        <ul>
-          <li>it enables <strong>access to any historical data</strong>;</li>
-          <li>
-            it requires
-            <strong>processing to acheive the historical state</strong>
-          </li>
-          <li>
-            <strong>plays along well with other patterns</strong>, such as
-            <NuxtLink class="underline text-gray-600" to="/learn/CQRS">
-              CQRS
-            </NuxtLink>
-            and
-            <NuxtLink
-              class="underline text-gray-600"
-              to="/learn/Asynchronous Messaging"
-            >
-              Asynchronous Messaging
-            </NuxtLink>
-            ,
-            <NuxtLink class="underline text-gray-600" to="/learn/Domain Event">
-              Domain Event
-            </NuxtLink>
-          </li>
-        </ul>
-      </section>
-    </div>
-  </LearnScaffold>
+    <template #consequences>
+      <ul>
+        <li>it enables <strong>access to any historical data</strong>;</li>
+        <li>
+          it requires
+          <strong>processing to acheive the historical state</strong>
+        </li>
+        <li>
+          <strong>plays along well with other patterns</strong>, such as
+          <NuxtLink class="underline text-gray-600" to="/learn/CQRS">
+            CQRS
+          </NuxtLink>
+          and
+          <NuxtLink
+            class="underline text-gray-600"
+            to="/learn/Asynchronous Messaging"
+          >
+            Asynchronous Messaging
+          </NuxtLink>
+          ,
+          <NuxtLink class="underline text-gray-600" to="/learn/Domain Event">
+            Domain Event
+          </NuxtLink>
+        </li>
+      </ul>
+    </template>
+  </PatternPage>
 </template>
 
 <style lang="postcss" scoped>
-.content {
-  @apply flex flex-col justify-around px-4;
-}
-
-.content > section {
-  @apply flex flex-col;
-  @apply my-4;
-}
-
-section > h2 {
-  @apply text-xl font-bold mt-2 mb-4;
-}
-
 section > ul {
   @apply list-disc list-inside;
 }
@@ -82,11 +55,5 @@ li {
 
 strong {
   @apply text-gray-600 text-xl;
-}
-
-@screen lg {
-  section {
-    @apply w-4/5 mx-auto;
-  }
 }
 </style>
