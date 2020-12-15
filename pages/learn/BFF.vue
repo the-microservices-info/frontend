d23861f9acc10bc1ce7b778294affd76e2921a6f
João Daniel <jotaf.daniel@gmail.com>
2020-12-15 03:18:12 -0300

    Add BFF and API Gateway
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ pages/learn/BFF.vue
@@ -0,0 +1,58 @@
+<template>
+  <PatternPage
+    name="BFF"
+    complete-name="Backends for Frontends"
+    :src="require('@/assets/images/patterns/BFFs.png')"
+    alt="Backends for Frontends"
+  >
+    <template #problem>
+      Consider you are developing an application that adopts Microservices.
+      Then, this application has <strong>multiple addresses</strong>, one for
+      each instance of each service. And all of this is
+      <strong>constantly changing</strong>, either by services being removed or
+      created, or by scaling up/down. Furthermore, this application has
+      <strong>many different types of clients</strong>, such as web, mobile and
+      even a public API, each with its own specific requirements of time
+      tolerance, protocols, and data.
+    </template>
+
+    <template #solution>
+      Implement
+      <strong>
+        multiple
+        <NuxtLink class="underline text-gray-600" to="/learn/API Gateway">
+          API Gateways
+        </NuxtLink>
+        ,
+      </strong>
+      one for each type of client.
+    </template>
+
+    <template #consequences>
+      <ul>
+        <li>
+          external clients access an interface that
+          <strong>abstracts the internal division</strong>;
+        </li>
+        <li>
+          each type of client deals with an interface that
+          <strong>fits well its needs</strong>.
+        </li>
+      </ul>
+    </template>
+  </PatternPage>
+</template>
+
+<style lang="postcss" scoped>
+section > ul {
+  @apply list-disc list-inside;
+}
+
+li {
+  @apply my-2;
+}
+
+strong {
+  @apply text-gray-600 text-xl;
+}
+</style>
