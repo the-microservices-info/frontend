37927f2c8292b5a1f14ce942849a1eb407e7ffa3
João Daniel <jotaf.daniel@gmail.com>
2020-12-16 00:59:56 -0300

    Redefine placeholder page
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- pages/learn/_id.vue
+++ pages/learn/_id.vue
@@ -1,6 +1,6 @@
 <template>
-  <PatternPage :name="$route.params.id" src="">
-    <template #problem></template>
+  <PatternPage :name="$route.params.id" :src="imageSrc">
+    <template #problem>(page under construction)</template>
     <template #solution></template>
     <template #consequences></template>
   </PatternPage>
@@ -9,3 +9,14 @@
 <style lang="postcss" scoped>
 @import '~/assets/css/pattern_page.css';
 </style>
+
+<script>
+export default {
+  computed: {
+    imageSrc() {
+      const noSpaceName = this.$route.params.id.replace(' ', '')
+      return require(`@/assets/images/patterns/${noSpaceName}.png`)
+    },
+  },
+}
+</script>
